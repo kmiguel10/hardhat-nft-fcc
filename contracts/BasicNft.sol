f3ca232e75c2cc6ff61d19eaf462457fb349bc3a
Kent Miguel <kentmiguel10@gmail.com>
2022-06-29 12:53:30 +0200

    BasicNft contract + deploy script

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -1,4 +1,32 @@
 //SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 pragma solidity ^0.8.7;
 
-contract BasicNft {}
+contract BasicNft is ERC721 {
+    string public constant TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Douggie", "DOUG") {
+        s_tokenCounter = 0;
+    }
+
+    //Calls _safeMint function of ERC721 contract and iterate id
+    function mintNft() public returns (uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+        return s_tokenCounter;
+    }
+
+    function tokenURI(
+        uint256 /*tokenId*/
+    ) public view override returns (string memory) {
+        //require(_exists(tokenId))
+        return TOKEN_URI;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
