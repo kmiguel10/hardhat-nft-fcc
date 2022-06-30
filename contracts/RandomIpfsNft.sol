27028ed49f7d1bd642bb5494814074cdfd3e3f64
Kent Miguel <kentmiguel10@gmail.com>
2022-06-30 13:31:31 +0200

    Update RandomIpfsNft.sol
    Implemented fulfilledrandomwords

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract RandomIpfsNft is VRFConsumerBaseV2 {
+contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
     //when we mint an NFT, we will trigger a chainlink VRF call to get us a random number
     //using that number we will get a random NFT
     //Pug (Super rare), shiba inu (sort of rare), St. Bernard (common)
@@ -20,12 +21,18 @@ contract RandomIpfsNft is VRFConsumerBaseV2 {
     uint16 private constant REQUET_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
+    //VRF Helpers
+    mapping(uint256 => address) public s_requestIdToSender;
+
+    //NFT Variables
+    uint256 public s_tokenCounter;
+
     constructor(
         address vrfCoordinatorV2,
         uint64 subscriptionId,
         bytes32 gasLane,
         uint32 callBackGasLimit
-    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN") {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_subscriptionId = subscriptionId;
         i_gasLane = gasLane;
@@ -40,12 +47,16 @@ contract RandomIpfsNft is VRFConsumerBaseV2 {
             i_callBackGasLimit,
             NUM_WORDS
         );
+
+        //set request id to sender
+        s_requestIdToSender[requestId] = msg.sender;
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
-        internal
-        override
-    {}
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        address dogOwner = s_requestIdToSender[requestId];
+        uint256 newTokenId = s_tokenCounter;
+        _safeMint(dogOwner, newTokenId);
+    }
 
-    function tokenURI(uint256) public {}
+    function tokenURI(uint256) public view override returns (string memory) {}
 }
