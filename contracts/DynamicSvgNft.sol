223edbd87517a0cabacf1ab3f8e86a9c288e8ae7
Kent Miguel <kentmiguel10@gmail.com>
2022-07-22 01:24:16 +0200

    Svg contract continuation ...
    converting svg to uri

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
 
 contract DynamicSvgNft is ERC721 {
     //mint
@@ -10,13 +11,36 @@ contract DynamicSvgNft is ERC721 {
     //show x image or show y image
 
     uint256 s_tokenCounter;
+    string private i_lowImageURI;
+    string private i_highImageURI;
+    string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
 
-    constructor() ERC721("Dynamic SVG NFT", "DSN") {
+    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
+    }
+
+    ///Takes any SVG and returns a URI
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
     }
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "URI Query for nonexistent token");
+        string memory imageURI = "hi!";
+
+        abi.encodePacked(
+            '{"name":"',
+            name(), // You can add whatever name here
+            '", "description":"An NFT that changes based on the Chainlink Feed", ',
+            '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+            imageURI,
+            '"}'
+        );
     }
 }
