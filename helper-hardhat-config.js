48f33c0ccb3a7f2777cba53f4624b0e250b2fbb1
Kent Miguel <kentmiguel10@gmail.com>
2022-07-25 16:23:57 +0200

    Update helper-hardhat-config.js
    Added mint fee

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -10,6 +10,7 @@ const networkConfig = {
         callbackGasLimit: "500000",
         interval: "30", //seconds,
         ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        mintFee: "10000000000000000", // 0.01 ETH
     },
     31337: {
         name: "hardhat",
